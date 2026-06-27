825de02357fd282ed5c5f80c3fb5184e0202186f
Thaís Miranda Nagahara <thais_miranda13@outlook.com>
2026-06-27 08:01:28 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -1,25 +1,27 @@
 package com.nelioalves.workshopmongo.resources;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity <List<User>> findAll() {
-        User maria = new User("1", "Maria White", "maria@email.com");
-        User alex = new User("2", "Alex Green", "alex@email.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
+
+        System.out.println("Tamanho da lista: " + list.size());
         return ResponseEntity.ok().body(list);
     }
 }
